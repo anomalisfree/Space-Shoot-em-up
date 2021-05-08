0511d48253f6c59cb7cbf72e09cae6d1efa10d8f
Aliaksandr <alisavets@provensolution.com>
2021-05-09 01:48:41 +0200

    add ufo explosion

--- Assets/Scripts/ScrollShooter/EnemyAI.cs
+++ Assets/Scripts/ScrollShooter/EnemyAI.cs
@@ -5,15 +5,20 @@ namespace ScrollShooter
     public class EnemyAI : MonoBehaviour
     {
         [SerializeField] private Health health;
+        [SerializeField] private GameObject explosion;
 
         private void Start()
         {
             health.HealthIsChanged += GetDamage;
         }
 
-        private void GetDamage(int obj)
+        private void GetDamage(int healthValue)
         {
-            Debug.Log("Enemy get damage");
+            if (healthValue <= 0)
+            {
+                Instantiate(explosion, health.transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
         }
     }
 }
